b7534099c518d74679d72c65d3cd3c2c1de3001c
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2021-01-22 12:00:00 +0900

    Lesson

---
+++ JavaFX/BreakoutMain.java
@@ -0,0 +1,80 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.animation.AnimationTimer;
+import javafx.scene.paint.Color;
+
+public class BreakoutMain extends Application
+{
+	//data
+	private BreakoutThread breakoutthread;
+
+	public static void main (String[] args)
+	{
+		launch(args);
+	}
+	@Override
+	public void start (Stage stage)
+	{
+		stage.setTitle("ブロック崩し");
+
+		//pane,scene,setScene()
+		Pane pane = new Pane();
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+
+		//Canvas, GraphicsContext, add()
+		Canvas canvas = new Canvas(640,480);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		pane.getChildren().add(canvas);
+
+		//thread;スレッドっを実行する
+		breakoutthread = new BreakoutThread(gc);
+		breakoutthread.start();
+		stage.show();
+	}
+}
+
+class BreakoutThread extends AnimationTimer
+{
+	//data
+	private GraphicsContext gc;
+	private int ball_x;		//ボールの場所;x
+	private int ball_y;		//ボールの場所;y
+	private int x_speed;	//ボールの速さ;x方向
+	private int y_speed;	//ボールの速さ;y方向
+
+	//method 
+	//constructor;コンストラクタ;newの時,１回だけ実行させる
+	BreakoutThread(GraphicsContext gc)
+	{
+		this.gc = gc;
+		ball_x  = 0;
+		ball_y = 0;
+		x_speed = 1;
+		y_speed = 1;
+	}
+
+	@Override 
+	public void handle(long time)
+	{
+		//画面表示をする、全部消す(全部白になる)
+		//gc.clearRect(0,0,640,480);
+		gc.setFill(Color.BLACK);
+		gc.fillOval(ball_x - 5, ball_y - 5, 10 ,10);
+
+		//ボールを移動させる
+		ball_x += x_speed;
+		ball_y += y_speed;
+
+		/*//文字を表示する
+		gc.fillText("count = " + count, 450,450);
+		gc.fillText("time = " + time, 450,500);
+
+		//回数をカウントする；表示する回数
+		count++; */
+	}
+}
--- JavaFX/Test.java
+++ JavaFX/Test.java
@@ -1,84 +1,62 @@
-import javafx.application.Application; 
+import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.animation.Animation;
-import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.util.Duration;
 
-
-public class setMouse extends Application
+public class Test extends Application
 {
-    public static void main (String[] args) 
+    public static void main (String[] args)
     {
         launch(args);
     }
+
     @Override
-    public void start (Stage stage ) throws Exception
+public void start(Stage stage) throws Exception
+{
+    View v = new View();
+    Scene scene = new Scene( v ,500,400);
+    stage.setScene(scene);
+    stage.setTitle("ブレックファースト");
+    stage.show();
+}
+}
+
+class View extends Group
+{
+    public View()
     {
-        View v = new View();
-        Scene scene = new Scene(v,500,500 );
+        //shape
+        Circle circle = new Circle(50,50,30);
+        circle.setFill(Color.BLUE);
+        Circle circle_suihei = new Circle(50,50,30);
+        circle_suihei.setFill(Color.RED);
 
-        //Mouse Control
-        scene.setOnMousePressed(e -> v.changeColor());
-        scene.setOnMouseClicked(e -> v.changeAnimation());
-        stage.setScene(scene);
-        stage.setTitle("Hello");
-        stage.show();
-    }
+        //animation,duration
+        TranslateTransition animation = new TranslateTransition(Duration.seconds(2), circle);
+        TranslateTransition animation2 = new TranslateTransition(Duration.seconds(2), circle_suihei);
+        //animation-from to
+        animation.setFromX(200);
+        animation.setFromY(300);
+        animation.setToX(200);
+        animation.setToY(10);
 
-    class View extends Group
-    {
-        Rectangle rect;
-        int flagColor = 0;
-        int flagAnimation = 0;
-        ScaleTransition animation;
-        public View()
-        {
+        animation2.setFromX(0);
+        animation2.setFromY(200);
+        animation2.setToX(400);
+        animation2.setToY(200);
+        //repeat
+        animation.setCycleCount(Animation.INDEFINITE);
+        animation2.setCycleCount(Animation.INDEFINITE);
 
-            rect = new Rectangle(100,100,100,100);
-            rect.setFill(Color.web("#232356"));
-
-            animation =  new ScaleTransition(Duration.seconds(2),rect);
-            animation.setFromX(0);
-            animation.setFromY(0);
-            animation.setToX(2);
-            animation.setToY(2);
-
-            animation.setCycleCount(Animation.INDEFINITE);
-            animation.setAutoReverse(true);
-            animation.play();
-            getChildren().add(rect);
-
-
-        }
-        public void changeColor()
-        {
-            
-            if ( flagColor == 0 ){
-            rect.setFill(Color.web("#111111"));
-            flagColor = 1;
-        }
-            else
-            {
-                rect.setFill(Color.web("#232356"));
-                flagColor = 0;
-            }
-        }
-        public void changeAnimation()
-        {
-            if ( flagAnimation == 0 )
-            {
-                animation.pause();
-                flagAnimation = 1;
-            }
-            else
-            {
-                animation.play();
-                flagAnimation = 0;
-            }
-        }
+        //play,add
+        animation.play();
+        animation2.play();
+        getChildren().add(circle);
+        getChildren().add(circle_suihei);
     }
 }
